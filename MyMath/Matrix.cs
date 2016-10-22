acd3b739787d47e37ec7dd35a1c3737ce56e7512
Erkan Taylan <erkantaylan@hotmail.com.tr>
2016-10-22 22:55:58 +0300

    commit

---
+++ MyMath/Matrix.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace MyMath {
+    public class Matrix<T> where T : new() {
+
+        private int rowCount;
+        private int columnCount;
+
+        public T[,] Values;
+
+        public Matrix(int rowCount, int columnCount) {
+            if (!IsNumericType(typeof(T))) {
+                throw new UnexpectedValueTypeException($"{typeof(T)} is not valid type. Only numeric types are allowed!");
+            }
+
+            RowCount = rowCount;
+            ColumnCount = columnCount;
+
+            Values = new T[rowCount, columnCount];
+        }
+
+        public int ColumnCount {
+            get { return columnCount; }
+            set {
+                if (value > 0) {
+                    columnCount = value;
+                }
+                else {
+                    throw new MatrixSizeError(MatrixSizeError.NotPossitiveMessage);
+                }
+            }
+        }
+
+        public int RowCount {
+            get { return rowCount; }
+            set {
+                if (value > 0) {
+                    rowCount = value;
+                }
+                else {
+                    throw new MatrixSizeError(MatrixSizeError.NotPossitiveMessage);
+                }
+            }
+        }
+
+        public Matrix<T> Multiply(Matrix<T> matrix) {
+            var result = new Matrix<T>(columnCount, matrix.rowCount);
+            for (int row = 0; row < rowCount; row++) {
+                for (int column = 0; column < columnCount; column++) {
+                    result.Values[row, column] = new T();
+                    for (int i = 0; i < matrix.rowCount; i++) {
+                        result.Values[row, column] = Add(result.Values[row, column], this.Values[row, i], matrix.Values[i, column]);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static T Add(T current, T y, T z) {
+            dynamic tx = current, ty = y, tz = z;
+            return tx + (ty * tz);
+        }
+
+        public Matrix<T> MultiplyAsync(Matrix<T> matrix) {
+            var result = new Matrix<T>(columnCount, matrix.rowCount);
+            for (int row = 0; row < rowCount; row++) {
+                for (int column = 0; column < columnCount; column++) {
+                    var r = row;
+                    var c = column;
+                    var t = new Thread(() => MultiplyAsync(matrix, result, r, c));
+                    t.Start();
+                }
+            }
+            return result;
+        }
+
+        private void MultiplyAsync(Matrix<T> matrix, Matrix<T> result, int row, int column) {
+            result.Values[row, column] = new T();
+            for (int i = 0; i < matrix.rowCount; i++) {
+                result.Values[row, column] = Add(result.Values[row, column], this.Values[row, i], matrix.Values[i, column]);
+            }
+        }
+
+        public bool IsSquare() {
+            return rowCount == columnCount;
+        }
+
+        /// <summary>
+        /// Determines if a type is numeric.  Nullable numeric types are considered numeric.
+        /// </summary>
+        /// <remarks>
+        /// Boolean is not considered numeric.
+        /// </remarks>
+        private static bool IsNumericType(Type type) {
+            while (true) {
+                if (type == null) {
+                    return false;
+                }
+
+                switch (Type.GetTypeCode(type)) {
+                    case TypeCode.Byte:
+                    case TypeCode.Decimal:
+                    case TypeCode.Double:
+                    case TypeCode.Int16:
+                    case TypeCode.Int32:
+                    case TypeCode.Int64:
+                    case TypeCode.SByte:
+                    case TypeCode.Single:
+                    case TypeCode.UInt16:
+                    case TypeCode.UInt32:
+                    case TypeCode.UInt64:
+                        return true;
+                    case TypeCode.Object:
+                        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)) {
+                            type = Nullable.GetUnderlyingType(type);
+                            continue;
+                        }
+                        return false;
+                    case TypeCode.Empty:
+                        break;
+                    case TypeCode.DBNull:
+                        break;
+                    case TypeCode.Boolean:
+                        break;
+                    case TypeCode.Char:
+                        break;
+                    case TypeCode.DateTime:
+                        break;
+                    case TypeCode.String:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                return false;
+            }
+        }
+
+        public override string ToString() {
+            var builder = new StringBuilder();
+            for (int i = 0; i < rowCount; i++) {
+                for (int j = 0; j < columnCount; j++) {
+                    builder.Append($"{Values[i, j],5:##00.####} ");
+                }
+                if (i + 1 != rowCount) {
+                    builder.Append(Environment.NewLine);
+                }
+            }
+            return builder.ToString();
+        }
+    }
+
+    public class UnexpectedValueTypeException : Exception {
+        public UnexpectedValueTypeException() {}
+
+        public UnexpectedValueTypeException(string message) : base(message) {}
+
+        public UnexpectedValueTypeException(string message, Exception inner) : base(message, inner) {}
+    }
+
+    public class MatrixSizeError : Exception {
+
+        public static string NotPossitiveMessage = "Matrix column size cannot be negative or zero!";
+
+        public MatrixSizeError() {}
+
+        public MatrixSizeError(string message) : base(message) {}
+
+        public MatrixSizeError(string message, Exception inner) : base(message, inner) {}
+    }
+}
---
+++ multi_threaded_matrix_multiplying/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using MyMath;
+
+namespace multi_threaded_matrix_multiplying {
+    internal static class Program {
+
+        private static readonly Random rd = new Random();
+
+        private static void Main(string[] args) {
+            int size = 100;
+            int max = 10;
+            Matrix<int> m1 = new Matrix<int>(size, size);
+            Matrix<int> m2 = new Matrix<int>(size, size);
+            //matrix.Values = new[,] {
+            //    {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+            //    {2, 3, 4, 5, 6, 7, 8, 9, 10, 1},
+            //    {3, 4, 5, 6, 7, 8, 9, 10, 1, 2},
+            //    {4, 5, 6, 7, 8, 9, 10, 1, 2, 3},
+            //    {5, 6, 7, 8, 9, 10, 1, 2, 3, 4},
+            //    {6, 7, 8, 9, 10, 1, 2, 3, 4, 5},
+            //    {7, 8, 9, 10, 1, 2, 3, 4, 5, 6},
+            //    {8, 9, 10, 1, 2, 3, 4, 5, 6, 7},
+            //    {9, 10, 1, 2, 3, 4, 5, 6, 7, 8},
+            //    {10, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+            //};
+
+            m1.Values = RandomSquareArray(size, max);
+            m2.Values = RandomSquareArray(size, max);
+
+            long best = long.MaxValue;
+            long worst = long.MinValue;
+            int testCount = 100;
+
+            for (int i = 0; i < testCount; i++) {
+                var currentTicks = MeasureFunctionRunTime(m1, m2).Ticks;
+                if (currentTicks < best) {
+                    best = currentTicks;
+                }
+                if (currentTicks > worst) {
+                    worst = currentTicks;
+                }
+            }
+
+            Console.WriteLine("- Without Threads");
+            Console.WriteLine($"Best Time  :{best,10}");
+            Console.WriteLine($"Worst Time :{worst,10}\n");
+
+            best = long.MaxValue;
+            worst = long.MinValue;
+            for (int i = 0; i < testCount; i++) {
+                var currentTicks = MeasureFunctionRunTimeAsync(m1, m2).Ticks;
+                if (currentTicks < best) {
+                    best = currentTicks;
+                }
+                if (currentTicks > worst) {
+                    worst = currentTicks;
+                }
+            }
+            Console.WriteLine("- With Threads");
+            Console.WriteLine($"Best Time  :{best,10}");
+            Console.WriteLine($"Worst Time :{worst,10}");
+
+
+
+            //            Console.WriteLine(matrix);
+            //            Console.WriteLine();
+
+            //            var m1 = matrix.Multiply(matrix);
+            //            Console.WriteLine(m1);
+            //            Console.WriteLine();
+
+            //            var m2 = matrix.MultiplyAsync(matrix);
+            //            Console.WriteLine(m2);
+            //            Console.WriteLine();
+
+            End();
+        }
+        
+
+        private static int[,] RandomSquareArray(int size, int max) {
+            var array = new int[size, size];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    array[i, j] = rd.Next(max);
+                }
+            }
+            return array;
+        }
+
+        private static void End() {
+            Console.WriteLine("End of Program");
+            Console.ReadKey(true);
+        }
+
+        private static TimeSpan MeasureFunctionRunTime(Matrix<int> m1, Matrix<int> m2) {
+            var stopwatch = Stopwatch.StartNew();
+            m1.Multiply(m2);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        private static TimeSpan MeasureFunctionRunTimeAsync(Matrix<int> m1, Matrix<int> m2)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            m1.MultiplyAsync(m2);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+    }
+}
